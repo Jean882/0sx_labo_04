d1f6883f57aecdbf00b5b360d0a6179eb0dc80b3
Jean-Philippe Darche <darchejeanphilippe@outlook.com>
2025-04-04 15:15:17 -0400

    Completed

--- Labo_04/Labo_04.ino
+++ Labo_04/Labo_04.ino
@@ -18,7 +18,10 @@ LCD_I2C lcd(0x27, 16, 2);
 HCSR04 distanceSensor(TRIGGER_PIN, ECHO_PIN);
 
 // States
-enum DoorState {CLOSED, OPENING, OPEN, CLOSING};
+enum DoorState { CLOSED,
+                 OPENING,
+                 OPEN,
+                 CLOSING };
 DoorState state = CLOSED;
 
 // Global variables
@@ -37,122 +40,118 @@ const unsigned long maxAngle = 170;
 
 #pragma region
 
-int getCurrentAngle() { // return map angle
-    return map(myStepper.currentPosition(), closedPosition, openPosition, minAngle, maxAngle);
+int getCurrentAngle() {  // return map angle
+  return map(myStepper.currentPosition(), closedPosition, openPosition, minAngle, maxAngle);
 }
 
-double measureDistance() { // return distance
-    return distanceSensor.dist();
+double measureDistance() {  // return distance
+  return distanceSensor.dist();
 }
 
 void displayLCD(double distance) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Dist: ");
-    lcd.print(distance);
-    lcd.print(" cm");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dist: ");
+  lcd.print(distance);
+  lcd.print(" cm");
 
-    lcd.setCursor(0, 1);
-    switch (state) {
-        case CLOSED:
-            lcd.print("Porte: Closed");
-            break;
-        case OPEN:
-            lcd.print("Porte: Open");
-            break;
-        default:
-            lcd.print("Porte: ");
-            lcd.print(getCurrentAngle());
-            lcd.print(" deg");
-            break;
-    }
+  lcd.setCursor(0, 1);
+  switch (state) {
+    case CLOSED:
+      lcd.print("Porte: Closed");
+      break;
+    case OPEN:
+      lcd.print("Porte: Open");
+      break;
+    default:
+      lcd.print("Porte: ");
+      lcd.print(getCurrentAngle());
+      lcd.print(" deg");
+      break;
+  }
 }
 
 void displaySerial(double distance) {
-    Serial.print("etd:2206160");
-    Serial.print(",dist:");
-    Serial.print(distance);
-    Serial.print(",deg:");
-    Serial.println(getCurrentAngle());
+  Serial.print("etd:2206160");
+  Serial.print(",dist:");
+  Serial.print(distance);
+  Serial.print(",deg:");
+  Serial.println(getCurrentAngle());
 }
 
 #pragma endregion
 
 void updateState(double distance) {
-    switch (state) {
-        case CLOSED:
-            if (distance < distanceThresholdOpen) {
-                state = OPENING;
-                myStepper.enableOutputs();
-                myStepper.moveTo(openPosition);
-            }
-            break;
+  switch (state) {
+    case CLOSED:
+      if (distance < distanceThresholdOpen) {
+        state = OPENING;
+        myStepper.enableOutputs();
+        myStepper.moveTo(openPosition);
+      }
+      break;
 
-        case OPENING:
-            if (myStepper.distanceToGo() == 0) {
-                state = OPEN;
-                myStepper.disableOutputs();
-            }
-            break;
+    case OPENING:
+      if (myStepper.distanceToGo() == 0) {
+        state = OPEN;
+        myStepper.disableOutputs();
+      }
+      break;
 
-        case OPEN:
-            if (distance > distanceThresholdClose) {
-                state = CLOSING;
-                myStepper.enableOutputs();
-                myStepper.moveTo(closedPosition);
-            }
-            break;
+    case OPEN:
+      if (distance > distanceThresholdClose) {
+        state = CLOSING;
+        myStepper.enableOutputs();
+        myStepper.moveTo(closedPosition);
+      }
+      break;
 
-        case CLOSING:
-            if (myStepper.distanceToGo() == 0) {
-                state = CLOSED;
-                myStepper.disableOutputs();
-            }
-            break;
-    }
+    case CLOSING:
+      if (myStepper.distanceToGo() == 0) {
+        state = CLOSED;
+        myStepper.disableOutputs();
+      }
+      break;
+  }
 }
 
-#pragma region setup-loop
+#pragma region setup - loop
 
 void setup() {
-    Serial.begin(115200);
-    lcd.begin();
-    lcd.backlight();
+  Serial.begin(115200);
+  lcd.begin();
+  lcd.backlight();
 
-    myStepper.setMaxSpeed(500);
-    myStepper.setAcceleration(250);
-    myStepper.setCurrentPosition(closedPosition);
-    myStepper.disableOutputs();
+  myStepper.setMaxSpeed(500);
+  myStepper.setAcceleration(250);
+  myStepper.setCurrentPosition(closedPosition);
+  myStepper.disableOutputs();
 
-    while (millis() <= 2000) {
+  while (millis() <= 2000) {
 
     lcd.setCursor(0, 0);
     lcd.print("2206160      ");
     lcd.setCursor(0, 1);
     lcd.print("Labo 4a      ");
-    
-
   }
 }
 
 void loop() {
-    unsigned long currentTime = millis();
-    static double distance = 0;
+  unsigned long currentTime = millis();
+  static double distance = 0;
 
-    if (currentTime - lastMeasureTime >= measureInterval) {
-        lastMeasureTime = currentTime;
-        distance = measureDistance();
-        updateState(distance);
-    }
+  if (currentTime - lastMeasureTime >= measureInterval) {
+    lastMeasureTime = currentTime;
+    distance = measureDistance();
+  }
+  updateState(distance);
+  myStepper.run();
 
-    myStepper.run();
-
-    if (currentTime - lastSerialTime >= serialInterval) {
-        lastSerialTime = currentTime;
-        displayLCD(distance);        
-        displaySerial(distance);
-    }
-
+  if (currentTime - lastSerialTime >= serialInterval) {
+    lastSerialTime = currentTime;
+    displayLCD(distance);
+    displaySerial(distance);
+  }
 }
 
 #pragma endregion
